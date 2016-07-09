446adaafd8e8d3aedf560baa27ad3f7461d19518
Luke Orland <orluke@gmail.com>
2016-07-09 19:54:38 -0400

    Add lein-exec

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -1,4 +1,5 @@
 {:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.2.0-SNAPSHOT"]
+                  [lein-exec "0.3.6"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
